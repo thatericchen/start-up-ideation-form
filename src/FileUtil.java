1462848e7bb660faa229a4918c945f8393d8f180
Eric Chen <112535058+thatericchen@users.noreply.github.com>
2023-04-14 23:49:29 -0400

    Update FileUtil.java

--- src/FileUtil.java
+++ src/FileUtil.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 import java.util.List;
 
 /**
- * @author
+ * @author Eric Chen
  * @version 1.0
  * This class contains methods to facilitate saving a List of StartUpIdea's to a file
  */
